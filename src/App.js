f96e03c26a7d8a54f039e579ca04a7db2f053943
klanc0403 <kelly@kellyisasinger.com>
2026-05-15 12:46:22 -0400

    Merged javascript into the React file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the Interactive Weather App.
-        </p>
-        <Weather />
-        
-      </header>
+      <Weather />
     </div>
   );
 }
