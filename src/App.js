cd9685f8ead8f0596f214a061acc8c1f1abd2452
klanc0403 <kelly@kellyisasinger.com>
2026-05-15 11:50:51 -0400

    Added Weather component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Weather />
         <a
           className="App-link"
           href="https://reactjs.org"
