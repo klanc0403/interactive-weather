213ce8f9820d4eda52b6c62fbf5ce1dbe431cc9c
klanc0403 <kelly@kellyisasinger.com>
2026-05-15 12:12:32 -0400

    Integreted SheCodes Weather API,

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 export default function Weather(){
+    let city = "New York";
+
+    useEffect(() => {
+        function handleResponse(response){
+            alert(`The weather in ${city} is ${response.data.temperature.current}ºF`)
+        }
+
+        let apiKey = "0232oa2bd084ect6f17c5fee93b97744";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+    }, [city]);
+
     return <h2>Hello from Weather!</h2>
 }
